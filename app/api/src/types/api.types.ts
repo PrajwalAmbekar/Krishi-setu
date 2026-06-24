cf7bfb0b9b16f5991e5590b67be12265943df736
PrajwalAmbekar <prajwalambekar.231ch039@nitk.edu.in>
2026-06-24 10:06:29 +0530

    Created ApiResponse<T> generic type with ApiErrorResponse and Helper Functions

---
+++ app/api/src/types/api.types.ts
@@ -0,0 +1,43 @@
+interface ApiResponse<T>{
+    success:boolean;
+    data:T;
+    message?:string;
+    timestamp?:number;
+}
+
+
+interface ApiErrorResponse{
+    success:boolean;
+    error:string;
+    statusCode:number;
+    message?:string;
+}
+
+
+interface ApiPaginationResponse<T> extends ApiResponse<T>{
+    pagination:{
+        page:number;
+        limit:number;
+        total:number;
+        totalPages:number;
+    }
+
+}
+
+function createApiResponse<T> (data:T, message?:string): ApiResponse<T> {
+    return {
+        success:true,
+        data,
+        message,
+        timestamp:Date.now()
+    }
+}
+
+function createApiErrorResponse(error:string,statusCode:number,message?:string): ApiErrorResponse{
+    return{
+        success:false,
+        error,
+        statusCode,
+        message,
+    }
+}
